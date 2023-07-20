3cb4570f9d9d78be01d55f5d23ef6b893b18c2a9
Charlie Cooper <charliecooper98@gmail.com>
2023-07-20 14:44:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+- https://github.com/dsherret/ts-morph/tree/latest/packages/ts-morph
+- supabase-graphl-example (nice way of fetching data)
+- https://github.com/snyk-snippets/modern-npm-package/tree/main
---
+++ src/types.ts
@@ -0,0 +1,35 @@
+export type Tables = Table[];
+
+export type Table = {
+  id: number;
+  schema: string;
+  name: string;
+  rls_enabled: boolean;
+  rls_forced: boolean;
+  replica_identity: string;
+  bytes: number;
+  size: string;
+  live_rows_estimate: number;
+  dead_rows_estimate: number;
+  comment: string | null;
+  primary_keys: PrimaryKey[];
+  relationships: Relationship[];
+};
+
+export type PrimaryKey = {
+  schema: string;
+  table_name: string;
+  name: string;
+  table_id: number;
+};
+
+export type Relationship = {
+  id: number;
+  constraint_name: string;
+  source_schema: string;
+  source_table_name: string;
+  source_column_name: string;
+  target_table_schema: string;
+  target_table_name: string;
+  target_column_name: string;
+};
