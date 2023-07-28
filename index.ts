78c78cd9fbbbc4de255f060a8beaee356db643c5
Charlie Cooper <charliecooper98@gmail.com>
2023-07-28 10:51:13 +0100

    small refactor

--- index.ts
+++ index.ts
@@ -4,37 +4,36 @@ import { ensureDir, remove, writeFile } from "fs-extra";
 import { parseHookFiles } from "./src/hooks";
 import { log } from "./src/log";
 import { parseSupabaseFile } from "./src/supabase";
-import { fetchTypes } from "./src/types";
+import { File, fetchTypes } from "./src/types";
 import { Project } from "ts-morph";
 
 const directory = process.env.IS_DEV ? "__backengine__" : "src/__backengine__";
 
+const fetchTableNames = (types: File): string[] => {
+  const project = new Project();
+  const sourceFile = project.addSourceFileAtPath(
+    `${directory}/${types.fileName}`
+  );
+
+  const node = sourceFile.getInterface("Database")!;
+  const tables = node.getProperty("public")!.getType().getProperty("Tables")!;
+
+  return tables
+    .getTypeAtLocation(node)
+    .getProperties()
+    .map((property) => property.getName());
+};
+
 const run = async () => {
   log("Starting code generation");
   const types = await fetchTypes();
+
   await remove(directory);
   await ensureDir(`${directory}/hooks`);
   await writeFile(`${directory}/${types.fileName}`, types.content);
 
-  const TableNames = (): string[] => {
-    const project = new Project();
-    const sourceFile = project.addSourceFileAtPath(
-      `${directory}/${types.fileName}`
-    );
-    const node = sourceFile.getInterface("Database")!;
-
-    const tables = node.getProperty("public")!.getType().getProperty("Tables")!;
-
-    return tables
-      .getTypeAtLocation(node)
-      .getProperties()
-      .map((p) => {
-        return p.getName();
-      });
-  };
-  const tableNames = TableNames();
-
   const supabaseFile = await parseSupabaseFile();
+  const tableNames = fetchTableNames(types);
   const hookFiles = await parseHookFiles(tableNames);
 
   await writeFile(
